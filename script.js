00201bbe4cbceec42c0a01af329a3294d51d65ed
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-26 19:58:10 -0700

    Teacher's version done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert('1')
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = [
+  "message one",
+  "message two",
+  "message three",
+  "message four",
+];
+
+const colors = ["red", "green", "blue", "orange"];
+
+button.addEventListener("click", () => createNotification());
+
+function createNotification() {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.style.color = colors[Math.floor(Math.random() * messages.length)];
+  notif.innerText = messages[Math.floor(Math.random() * messages.length)];
+  toasts.appendChild(notif);
+  setTimeout(() => {
+    toasts.removeChild(notif);
+  }, 3000);
+}
